b7072ddc9b24d8e6c8e57d7b5df10e6aef45def6
Muhammad Tarek <muhammadtarek96@gmail.com>
2017-07-15 23:40:56 +0200

    Change background image from Unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+
+import './main.css';
+import getRandomImage from '../apis/Unsplash';
+
+// Styled Components
+let Body = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+let AppContainer = styled.div`min-width: 1100px;`;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgImage: ''
+    };
+  }
+
+  componentDidMount() {
+    // Calling Unsplash api and getting a random photo
+    getRandomImage().then(image => {
+      let body = document.getElementsByTagName('body');
+      body[0].style.backgroundImage = `url(${image})`;
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>my-dashboard</h1>
-      </div>
+      <Body>
+        <AppContainer>
+          <h1>Good Mornining, Muhammad</h1>
+        </AppContainer>
+      </Body>
     );
   }
 }
